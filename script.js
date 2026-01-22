9419b6eb2414e93b9c6a37793e1603140847720a
Jade <jgoodw46@wgu.edu>
2026-01-21 17:28:06 -0800

    Introduces JavaScript functions for calculator operations

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let operand1 = undefined;
+let operator = undefined;
+let operand2 = undefined;
+
+/* Function input: two numbers and an operator
+Function output: result of function called on numbers*/
+function operate(num1, num2, operator) {
+
+}
+
+operand1 = 9;
+operand2 = 3;
+console.log(add(operand1, operand2));
+console.log(subtract(operand1, operand2));
+console.log(multiply(operand1, operand2))
+console.log(divide(operand1, operand2))
